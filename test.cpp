9108ceff1cd6f21522e329aaf766efbc41a4f30a
Murray Fordyce <unDeadHerbs@gmail.com>
2019-02-11 21:33:06 -0600

    tested

--- test.cpp
+++ test.cpp
@@ -3,4 +3,18 @@ using std::cout;
 using std::endl;
 #include "tokenizer.hpp"
 
-int main(int argc, char *argv[]) { cout << "Starting tokenizer test." << endl; }
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    cout << "Usage: " << argv[0] << " [Filename]" << endl;
+    return 0;
+  }
+  cout << "Starting tokenizer test." << endl
+       << endl
+       << "| Token Type | Token Value" << endl
+       << "+------------+-- - - -  -  -" << endl;
+  initialize(argv[1]);
+  Token t;
+  while ((t = get_token()))
+    cout << t << endl;
+  cleanup();
+}
