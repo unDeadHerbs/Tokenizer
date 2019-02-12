09500fbe045a354e82dafe48d8e4712dadfc27ff
Murray Fordyce <unDeadHerbs@gmail.com>
2019-02-12 11:48:53 -0600

    reorginised some things together

--- test.cpp
+++ test.cpp
@@ -2,6 +2,10 @@
 using std::cout;
 using std::endl;
 #include "tokenizer.hpp"
+using std::ostream;
+using std::string;
+#include <iomanip>
+using std::setw;
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -14,9 +18,57 @@ int main(int argc, char *argv[]) {
        << "+------------+-- - - -  -  -" << endl;
   if (!initialize(argv[1]))
     exit(-1);
-  Token t(TOKEN(0), 0, 0, "");
-  while ((t = get_token()))
+  while (Token t = get_token())
     cout << t << endl;
   cleanup();
   cout << "+------------+-- - - -  -  -" << endl;
+}
+
+string TOKEN_name(TOKEN t) {
+#define echocase(x)                                                            \
+  case x:                                                                      \
+    return #x
+  switch (t) {
+    echocase(EOF_T);
+    echocase(UNKNOWN);
+    echocase(ERROR);
+    echocase(PLUS);
+    echocase(MINUS);
+    echocase(MULT);
+    echocase(DIV);
+    echocase(EXP);
+    echocase(LESS);
+    echocase(LESS_EQ);
+    echocase(GREATER);
+    echocase(GREATER_EQ);
+    echocase(EQUAL);
+    echocase(NOT_EQUAL);
+    echocase(ASSIGN);
+    echocase(NOT);
+    echocase(LPAREN);
+    echocase(RPAREN);
+    echocase(LBRACE);
+    echocase(RBRACE);
+    echocase(LBRACKET);
+    echocase(RBRACKET);
+    echocase(AND);
+    echocase(OR);
+    echocase(DOT);
+    echocase(AT);
+    echocase(COLON);
+    echocase(SEMICOLON);
+    echocase(COMMA);
+    echocase(IDENT);
+    echocase(INT);
+    echocase(STRING);
+    echocase(REAL);
+#undef echocase
+  default:
+    return "bad token";
+  }
+}
+
+ostream &operator<<(ostream &o, Token t) {
+  return o << "|" << setw(11) << TOKEN_name(t.id) << " | '" << t.text << "' at "
+           << t.line << ":" << t.col;
 }
--- tokenizer.cpp
+++ tokenizer.cpp
@@ -1,8 +1,5 @@
 #include "tokenizer.hpp"
-using std::ostream;
 using std::string;
-#include <iomanip>
-using std::setw;
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -344,52 +341,3 @@ Token get_token() {
   return Token(EOF_T, r, c, "");
 }
 bool cleanup() { return 0 == fclose(fp); }
-
-string TOKEN_name(TOKEN t) {
-#define echocase(x)                                                            \
-  case x:                                                                      \
-    return #x
-  switch (t) {
-    echocase(EOF_T);
-    echocase(UNKNOWN);
-    echocase(ERROR);
-    echocase(PLUS);
-    echocase(MINUS);
-    echocase(MULT);
-    echocase(DIV);
-    echocase(EXP);
-    echocase(LESS);
-    echocase(LESS_EQ);
-    echocase(GREATER);
-    echocase(GREATER_EQ);
-    echocase(EQUAL);
-    echocase(NOT_EQUAL);
-    echocase(ASSIGN);
-    echocase(NOT);
-    echocase(LPAREN);
-    echocase(RPAREN);
-    echocase(LBRACE);
-    echocase(RBRACE);
-    echocase(LBRACKET);
-    echocase(RBRACKET);
-    echocase(AND);
-    echocase(OR);
-    echocase(DOT);
-    echocase(AT);
-    echocase(COLON);
-    echocase(SEMICOLON);
-    echocase(COMMA);
-    echocase(IDENT);
-    echocase(INT);
-    echocase(STRING);
-    echocase(REAL);
-#undef echocase
-  default:
-    return "bad token";
-  }
-}
-
-ostream &operator<<(ostream &o, Token t) {
-  return o << "|" << setw(11) << TOKEN_name(t.id) << " | token '" << t.text
-           << "' at " << t.line << ":" << t.col;
-}
