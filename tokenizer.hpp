70918df5dd5ae1f79da5948ce183268f224808e9
Murray Fordyce <Murray.Fordyce@gmail.com>
2019-04-17 02:10:21 +0000

    formmating

--- tokenizer.hpp
+++ tokenizer.hpp
@@ -4,53 +4,54 @@
 #include <string>
 
 enum TOKEN {
-	    EOF_T	= 0 ,
-	    UNKNOWN	=-1 ,
-	    ERROR	=-2 ,
-	    PLUS	='+',
-	    MINUS	='-',
-	    MULT	='*',
-	    DIV		='/',
-	    EXP		='^',
-	    LESS	='<',
-	    LESS_EQ	= 1 , // <=
-	    GREATER	='>',
-	    GREATER_EQ	= 2, // >=
-	    EQUAL	='=',
-	    NOT_EQUAL	= 3 , // ~=
-	    ASSIGN	= 4 , // <-
-	    NOT		='!',
-	    LPAREN	='(',
-	    RPAREN	=')',
-	    LBRACE	='{',
-	    RBRACE	='}',
-	    LBRACKET	='[',
-	    RBRACKET	=']',
-	    AND		='&',
-	    OR		='|',
-	    DOT		='.',
-	    AT		='@',
-	    COLON	=':',
-	    SEMICOLON	=';',
-	    COMMA	=',',
-	    IDENT	= 5 ,
-	    INT		= 6 ,
-	    STRING	= 7 ,
-	    REAL	= 8 };
+	EOF_T = 0,
+	UNKNOWN = -1,
+	ERROR = -2,
+	PLUS = '+',
+	MINUS = '-',
+	MULT = '*',
+	DIV = '/',
+	EXP = '^',
+	LESS = '<',
+	LESS_EQ = 1,  // <=
+	GREATER = '>',
+	GREATER_EQ = 2,  // >=
+	EQUAL = '=',
+	NOT_EQUAL = 3,  // ~=
+	ASSIGN = 4,     // <-
+	NOT = '!',
+	LPAREN = '(',
+	RPAREN = ')',
+	LBRACE = '{',
+	RBRACE = '}',
+	LBRACKET = '[',
+	RBRACKET = ']',
+	AND = '&',
+	OR = '|',
+	DOT = '.',
+	AT = '@',
+	COLON = ':',
+	SEMICOLON = ';',
+	COMMA = ',',
+	IDENT = 5,
+	INT = 6,
+	STRING = 7,
+	REAL = 8
+};
 
-struct Token{
-  TOKEN id;
-  int line;
-  int col;
-  std::string text;
-public:
-  friend std::ostream& operator<<(std::ostream &o, Token t);
-  Token(TOKEN t,int l,int c,std::string tx):id(t),line(l),col(c),text(tx){}
-  operator bool()const{return id!=EOF_T;
-  }
+struct Token {
+	TOKEN id;
+	int line;
+	int col;
+	std::string text;
+
+ public:
+	friend std::ostream& operator<<(std::ostream& o, Token t);
+	Token(TOKEN t, int l, int c, std::string tx)
+	    : id(t), line(l), col(c), text(tx) {}
+	operator bool() const { return id != EOF_T; }
 };
 bool initialize(char* filename);
 Token get_token();
 bool cleanup();
 #endif
-
